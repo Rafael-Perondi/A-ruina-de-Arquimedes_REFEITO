e06d6743e3da92577c00354e553f512f3466c5c0
Rafael-Perondi <159496674+Rafael-Perondi@users.noreply.github.com>
2024-04-21 23:31:25 -0300

    Add files via upload

--- src/scripts/scenes/mainScene.js
+++ src/scripts/scenes/mainScene.js
@@ -1,3 +1,6 @@
+
+
+
 export class MainScene extends Phaser.Scene {
     constructor() {
         super({ key: 'MainScene' });
@@ -6,24 +9,41 @@ export class MainScene extends Phaser.Scene {
     }
 
     
-    preload() { // Carregando os arquivos para o phaser
-        this.load.image("groundImage", "assets/map/tiles/groundTileset.png")
+
+
+    preload() { // Carregando os arquivos para o phaser | Loading files to phaser
+        this.load.image("groundImage", "assets/map/tiles/groundTileset.png")   
         this.load.image("grassImage", "assets/map/tiles/grassTileset.png")
         this.load.image("waterImage", "assets/map/tiles/waterTileset.png")
-        this.load.tilemapTiledJSON("map", "assets/map/map.json") 
+        this.load.tilemapTiledJSON("map", "assets/map/map.json")
     }
 
-    create() {
+    create() {  /* Mostrando os arquivos carregados para a tela
+                * Showing loaded files to the screen
+                */
+
         const map = this.make.tilemap({ key: "map" }) 
+
+
+        /**
+         * @param {string} ground - Tileset a ser preenchido | Tileset to be filled
+         * @param {string} groundImage - Imagem a preencher tileset solicitado | Image to fill requested tileset 
+         */
 
         const tilesetGround = map.addTilesetImage("ground", "groundImage")
         const tilesetGrass = map.addTilesetImage("grass", "grassImage")
         const tilesetWater = map.addTilesetImage("water", "waterImage")
 
+        /**
+         * 
+         */
+
         const grass = map.createLayer("grass", tilesetGrass, 0, 0) 
         this.water = map.createLayer("water", tilesetWater, 0, 0)
 
         this.water.setCollisionByProperty({ collider: true})
+
+
         
 
     }
